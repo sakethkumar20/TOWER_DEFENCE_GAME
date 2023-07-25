840a3619f2884c2b6ba456212299af12bcaa7e9f
saketh kumar <sakethkumarpeddi@gmail.com>
2023-07-26 02:22:44 +0530

    first commit

---
+++ levels/practice.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="practice" tilewidth="48" tileheight="48" spacing="1" margin="1" tilecount="196" columns="14">
+ <image source="level.png" width="720" height="720"/>
+</tileset>
